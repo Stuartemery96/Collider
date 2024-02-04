0c52b2e78f3f205fa23f14931bf11a06cb3bae34
nicodrose <ndesro@gmail.com>
2024-02-04 14:27:37 -0800

    Includes profile page

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('events/create/', views.EventCreate.as_view(), name='events_create'),
     path('events/<int:event_id>/collide_create/', views.CollideCreate.as_view(), name='collide_create'),
     path('collides/<int:collide_id>/', views.collides_detail, name='collides_detail'),
-    path('collides/<int:collide_id>/add_rsvp', views.rsvp_create, name='add_rsvp'),
+    path('collides/<int:collide_id>/add_rsvp/', views.rsvp_create, name='add_rsvp'),
+    path('users/', views.user_profile, name='user_profile'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
